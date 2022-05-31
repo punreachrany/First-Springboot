f869e7cb8f63aa168f6ea97be73ed2b7da2ecfd9
punreachrany <punreach.rany@yahoo.com>
2022-05-31 16:03:09 +0900

    Connecting with Postgres Database

--- src/main/java/com/example/codeCamp/dao/PersonDataAccessService.java
+++ src/main/java/com/example/codeCamp/dao/PersonDataAccessService.java
@@ -1,8 +1,9 @@
 package com.example.codeCamp.dao;
 
 import com.example.codeCamp.model.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.Optional;
@@ -11,6 +12,13 @@ import java.util.UUID;
 @Repository("postgres")
 public class PersonDataAccessService implements PersonDao{
 
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public PersonDataAccessService(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
     @Override
     public int insertPerson(UUID id, Person person) {
         return 0;
@@ -18,12 +26,35 @@ public class PersonDataAccessService implements PersonDao{
 
     @Override
     public List<Person> selectAllPeople() {
-        return List.of(new Person(UUID.randomUUID(),"From Postgres DB"));
+        final String sql = "SELECT id, name FROM person";
+        List<Person> people = jdbcTemplate.query(sql, (rs, rowNum) -> {
+            UUID id = UUID.fromString(rs.getString("id"));
+            String name = rs.getString("name");
+            return new Person(
+                    id,
+                    name
+            );
+        });
+        return people;
     }
 
     @Override
     public Optional<Person> selectPersonById(UUID id) {
-        return Optional.empty();
+        final String sql = "SELECT id, name FROM person WHERE id = ?";
+
+        Person person = jdbcTemplate.queryForObject(
+                sql,
+                new Object[]{id},
+                (rs, rowNum) -> {
+                    UUID personId = UUID.fromString(rs.getString("id"));
+                    String name = rs.getString("name");
+                    return new Person(
+                            personId,
+                            name
+                    );
+        });
+
+        return Optional.ofNullable(person);
     }
 
     @Override
